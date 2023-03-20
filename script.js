f8dd6d2a967911f8ce862655971219077678c656
PMcManusphys <davington44@gmail.com>
2023-03-20 13:11:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -159,16 +159,16 @@ function saveValue() {
     
     
     .then(response => response.blob())
-            .then(blob => {
-                var url = window.URL.createObjectURL(blob);
-                var a = document.createElement('a');
-                a.href = url;
-                a.download = "filename.doc"; // find a way to pass in the file name as well
-                document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-                a.click();    
-                a.remove();  //afterwards we remove the element again 
-                URL.revokeObjectURL
-            })
+    .then(blob => {
+        var url = window.URL.createObjectURL(blob);
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = "filename.doc"; // find a way to pass in the file name as well
+        document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+        a.click();    
+        a.remove();  //afterwards we remove the element again 
+        URL.revokeObjectURL();
+    })
     .catch(err => console.error(err));
     //or
     //.then( blob => {
